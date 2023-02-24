0cf71a61745d073af8e8ffb410d7b67bbaec5bb7
BHARAT KUMAR PRAJAPAT <123304396+bharatprajapat014@users.noreply.github.com>
2023-02-24 11:55:44 +0530

    Update PR_2_SWAPARRAY.java

--- ARRAY-60-PROGRAM/PR_2_SWAPARRAY.java
+++ ARRAY-60-PROGRAM/PR_2_SWAPARRAY.java
@@ -1,1 +1,16 @@
+import java.util.Arrays;
+
+public class swap {
+    static void swap(int[] myArray,int i,int j)
+    {
+     int temp = myArray[i];
+     myArray[i]=myArray[j];
+     myArray[j]=temp;
+    }
+    public static void main(String[] args) {
+        int[] myArray = {1, 2, 3, 4, 5};
+        swap(myArray,0,4);
+        System.out.println(Arrays.toString(myArray));
+    }
+}
 
