38b2f80fa833cd190596f601fb3975b8e83e75fa
BHARAT KUMAR PRAJAPAT <123304396+bharatprajapat014@users.noreply.github.com>
2023-02-25 22:31:39 +0530

    Create PR_11_OddpoSition.java

---
+++ ARRAY-60-PROGRAM/PR_11_OddpoSition.java
@@ -0,0 +1,10 @@
+public class evenposition {
+    public static void main(String[] args) {
+        int [] arr = new int [] {1, 2, 3, 4, 5};
+        System.out.println("Elements of given array present on even position: ");
+        for (int i = 0; i < arr.length; i = i+2)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+}
