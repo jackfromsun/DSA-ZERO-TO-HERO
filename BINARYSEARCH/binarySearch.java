ff4a3d8a29b9b75c2efb544648d2d97c20cb279e
BHARAT KUMAR PRAJAPAT <123304396+bharatprajapat014@users.noreply.github.com>
2023-03-02 23:47:06 +0530

    Create binarySearch.java

---
+++ BINARYSEARCH/binarySearch.java
@@ -0,0 +1,39 @@
+public class binarySearhh {
+    public static void main(String[] args) {
+        int[] arr = {1,3,5,7,4};
+        int target = 7;
+        int index = binarysearch(arr,target);
+        if (index == -1)
+        {
+            System.out.println("0");
+        }
+        else
+        {
+            System.out.println(index);
+        }
+    }
+    static int binarysearch(int[] arr,int target)
+    {
+      int left=0;
+      int right=arr.length-1;
+      while(left<=right)
+      {
+          int mid = left+(right-left)/2;
+          if (arr[mid]==target)
+          {
+              return mid;
+          }
+          else if (arr[mid]<target)
+          {
+              left = mid+1;
+          }
+          else
+          {
+              right=mid-1;
+          }
+      }
+
+      return -1;
+
+    }
+}
