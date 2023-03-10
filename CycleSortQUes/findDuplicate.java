ac6a5a137ae6ecd239d938e916428d77e6dc7241
BHARAT KUMAR PRAJAPAT <123304396+bharatprajapat014@users.noreply.github.com>
2023-03-10 22:44:54 +0530

    Create findDuplicate.java

---
+++ CycleSortQUes/findDuplicate.java
@@ -0,0 +1,35 @@
+public class findDuplicateIncsort {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 3,3, 5, 4};
+        System.out.println(duplicate(arr));
+
+    }
+    static int duplicate(int[] arr)
+    {
+        int i=0;
+        while(i< arr.length)
+        {
+            if(arr[i]!=i+1)
+            {
+                int correct = arr[i]+1;
+                if (arr[i]!=arr[correct])
+                {
+                    swap(arr,i,correct);
+                }
+                else {
+                    return arr[i];
+                }
+            }
+            else{
+                i++;
+            }
+        }
+        return -1;
+    }
+    static void swap(int[] arr,int first,int second)
+    {
+        int temp = arr[first];
+        arr[first]= arr[second];
+        arr[second] = temp;
+    }
+}
